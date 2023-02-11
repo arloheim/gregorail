a41272a1856450e762f21cd6699c6af69e36303d
Danae Dekker <info@danaedekker.com>
2023-02-11 16:30:55 +0100

    Changed /gadmin to /gregorail; reload the plugin with /gregorail reload command

--- src/main/java/dev/danae/gregorail/RailPlugin.java
+++ src/main/java/dev/danae/gregorail/RailPlugin.java
@@ -1,6 +1,7 @@
 package dev.danae.gregorail;
 
 import dev.danae.gregorail.commands.CommandGroupHandler;
+import dev.danae.gregorail.handlers.admin.AdminReloadCommand;
 import dev.danae.gregorail.handlers.admin.AdminVersionCommand;
 import dev.danae.gregorail.handlers.cart.CartSetCommand;
 import dev.danae.gregorail.handlers.cart.CartUnsetCommand;
@@ -9,6 +10,7 @@ import dev.danae.gregorail.handlers.locate.LocateBlockCommand;
 import dev.danae.gregorail.handlers.rail.RailSwitchCommand;
 import dev.danae.gregorail.handlers.rail.RailSwitchIfCommand;
 import dev.danae.gregorail.util.location.LocationUtils;
+import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -40,22 +42,38 @@ public final class RailPlugin extends JavaPlugin
   }
   
   
+  // Reload the plugin
+  public void reload()
+  {
+    // Load the configuration
+    this.loadConfiguration();
+  }
+  
+  
   // Load the configuration
   private void loadConfiguration()
   {
     this.saveDefaultConfig();
+    this.reloadConfig();
     
     var generalConfig = this.getConfig().getConfigurationSection("general");
     if (generalConfig != null)
     {
       LocationUtils.blockSearchRadius = generalConfig.getInt("block-search-radius", 10);
+      this.getLogger().log(Level.INFO, String.format("general.block-search-radius = %d", LocationUtils.blockSearchRadius));
+      
       LocationUtils.entitySearchRadius = generalConfig.getInt("entity-search-radius", 10);
+      this.getLogger().log(Level.INFO, String.format("general.entity-search-radius = %d", LocationUtils.entitySearchRadius));
     }
   }
   
   // Load the command handlers
   private void loadCommandHandlers()
   {
+    this.getCommand("gregorail").setExecutor(new CommandGroupHandler(this)
+      .registerSubcommand("reload", new AdminReloadCommand(this))
+      .registerSubcommand("version", new AdminVersionCommand(this)));
+    
     this.getCommand("gcart").setExecutor(new CommandGroupHandler(this)
       .registerSubcommand("set", new CartSetCommand(this))
       .registerSubcommand("unset", new CartUnsetCommand(this)));
@@ -67,8 +85,5 @@ public final class RailPlugin extends JavaPlugin
     this.getCommand("glocate").setExecutor(new CommandGroupHandler(this)
       .registerSubcommand("block", new LocateBlockCommand(this))
       .registerSubcommand("cart", new LocateCartCommand(this)));
-    
-    this.getCommand("gadmin").setExecutor(new CommandGroupHandler(this)
-      .registerSubcommand("version", new AdminVersionCommand(this)));
   }
 }
